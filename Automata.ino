dbd698e6cbbf8c86103ff9217c1875abd9476c5c
Jesse Meijer <j.m.meijer@outlook.com>
2018-04-26 01:04:11 +0200

    Defined constants and variabels

--- Automata.ino
+++ Automata.ino
@@ -5,6 +5,13 @@
 #include <Adafruit_NeoPixel.h>
 #include <NewPing.h>
 
+#define TRIGGER_PIN A0
+#define ECHO_PIN A1
+#define MAX_DISTANCE 53
+
+unsigned int pingSpeed = 50;
+unsigned long pingTimer;
+
 void setup() {
 
 }
